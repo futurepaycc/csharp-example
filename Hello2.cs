02d0fbe007d61ea6bcf2a2d2ed589ac510fc9449
liunix <liuyifan.com@gmail.com>
2017-07-11 21:01:18 +0800

    add event and task

--- Hello2.cs
+++ Hello2.cs
@@ -4,8 +4,24 @@ using System.Windows.Forms;
 
 public class Hello2 : Form {
     public Hello2() {
-        this.Text = "最好的窗口示例啊";
-        this.Size = new Size(400,300);
+        this.Text = "简单示例";
+        this.Size = new Size(400, 300);
+
+        Button button = new Button();
+        button.Location = new Point(30, 20);
+        button.Text = "close"; //汉字有乱码
+        button.Click += new EventHandler(this.onClick);
+        button.MouseEnter += new EventHandler(this.onEnter);
+        this.Controls.Add(button);
+        this.CenterToScreen();
+    }
+
+    void onClick(Object sender, EventArgs e) {
+        this.Close();
+    }
+
+    void onEnter(Object sender, EventArgs e) {
+        Console.WriteLine("mouse enter");
     }
 }
 
