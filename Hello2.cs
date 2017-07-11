f3c250c4d45c70147b0fce8a1147b65768e23dc9
liunix <liuyifan.com@gmail.com>
2017-07-11 19:29:47 +0800

    add test

--- Hello2.cs
+++ Hello2.cs
@@ -5,6 +5,9 @@ public class Hello2 : Form {
     public Hello2() {
         this.Text = "最好的窗口示例啊";
     }
+}
+
+public class MainClass {
     public static void Main() {
         Application.Run(new Hello2());
     }
