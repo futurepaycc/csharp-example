4292854dd67f150a8db79b73bfe8ec6e82526b6d
liunix <liuyifan.com@gmail.com>
2017-07-11 20:40:28 +0800

    增加多任务配置

--- Hello2.cs
+++ Hello2.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 public class Hello2 : Form {
     public Hello2() {
         this.Text = "最好的窗口示例啊";
+        this.Size = new Size(400,300);
     }
 }
 
