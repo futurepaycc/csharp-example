58791ee351b6b7a04aba815805a9f51e9c62466c
liunix <liuyifan.com@gmail.com>
2017-07-16 19:21:04 +0800

    reflect string

--- Reflect.cs
+++ Reflect.cs
@@ -1,3 +1,16 @@
 using System;
 using System.Reflection;
 
+public class Reflect1{
+    public static void Main(){
+        Type t = typeof(string);
+        MethodInfo[] methods =  t.GetMethods();
+        foreach(MethodInfo info in methods){
+            if(info.Name.Equals("ToUpper")){ //有两个ToUpper,如何区分?
+                var result = info.Invoke("asdf",null);
+                Console.WriteLine(result);
+                break;
+            }
+        }
+    }
+}
